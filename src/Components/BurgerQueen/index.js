7e4668d015a5d60e5478c92c8dd6c9503a1fdaeb
unknown <nutrimary.77@gmail.com>
2020-02-25 16:41:46 -0600

    first commit

---
+++ src/Components/BurgerQueen/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BurgerQueen extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Burger Queen</h1>
+            </div>
+        )
+    }
+}

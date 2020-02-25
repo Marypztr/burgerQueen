37da4a4df94f7ef9dd3c353e7c8ec0ec0b25d99f
unknown <nutrimary.77@gmail.com>
2020-02-25 17:01:56 -0600

    maquetado login

--- src/Components/BurgerQueen/index.js
+++ src/Components/BurgerQueen/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import styles from "./BurgerQueen.module.css"
+
+/* 
+paleta de colores 
+        #F6D8AE
+        #2E4057
+        #083D77
+        #DA4167
+        #F4D35E
+*/
 
 export default class BurgerQueen extends Component {
     render() {
         return (
-            <div>
-                <h1>Burger Queen</h1>
+            <div className={styles.Home}>
+                <div className={styles. positionLogin}>
+                    <h1> Bienvenido a BurgerQueen</h1>
+                    <h3>Iniciar sesión</h3>
+                    <p> <input type="checkbox"/>Soy Gerente</p>
+                    <p> <input type="checkbox"/>Soy Mesero</p>
+                </div>
             </div>
         )
     }
